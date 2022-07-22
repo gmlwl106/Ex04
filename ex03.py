a97959a7dfccf9a98edd80e733d84a77549e26b2
gmlwl106 <66778894@naver.com>
2022-07-22 12:20:12 +0900

    파이썬 pip

--- ex03.py
+++ ex03.py
@@ -2,30 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import util
 
-#랭킹 추출
-#1.사이트에 요청 응답(html)을 받기 - requests
-url = "https://movie.naver.com/movie/sdb/rank/rmovie.naver"
+#댓글 추출
+
+#1.사이트에 응답(html)을 받기 - requests
+url = "https://movie.daum.net/moviedb/grade?movieId=132346"
 
 #요청하기
-response = requests.get(url)
+response = requests.get(url);
+
 html = response.text
+#print(html)
 
 #2.필요한 태그 추출하기 - BeautifulSoup4
 soup = BeautifulSoup(html, "html.parser")
-tags = soup.select(".tit3>a")
-
-for index, tag in enumerate(tags):
-    #랭킹
-    rank = index+1
-    #영화제목
-    title = tag.text
-    #포스터
-    sub_page_url = tag["href"]
-    sub_url = "https://movie.naver.com/"+sub_page_url
-    filePath = util.imgDown(sub_url) #서브페이지에서 포스터 수집
-    #sub_page_url 요청 -> 응답(html)
-    #필요한 img_url 추출
-    #img_url 요청 -> 응답
-    #C:\\javaStudy\\upload\\movie\\ 저장
-    #파일경로
-    print(rank, title, sub_page_url)
+tags = soup.select(".cmt_info")
+print(tags)
---
+++ ex04.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+#브라우저 생성
+wd = webdriver.Chrome("C:\\javaStudy\\chromedriver\\chromedriver.exe")
+
+#사이트 요청
+url = "https://movie.daum.net/moviedb/grade?movieId=132346"
+wd.get(url)
+wd.implicitly_wait(10) #최대 10초 로딩 끝나면 그 전에 끝남
+
+#더보기 클릭
+while(True):
+    try:
+        wd.find_element(By.CSS_SELECTOR, ".alex_more").click()
+        wd.implicitly_wait(2)
+    except:
+        print("마지막")
+        break
+
+
+
+#필요한 태그 추출하기 webdriver
+cmt_tags = wd.find_elements(By.CSS_SELECTOR, ".cmt_info")
+
+for i, cmt_tag in enumerate(cmt_tags):
+    point = cmt_tag.find_element(By.CSS_SELECTOR, ".ratings").text
+
+    try:
+        cmt = cmt_tag.find_element(By.CSS_SELECTOR, "li p").text
+    except:
+        cmt = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        
+    date = cmt_tag.find_element(By.CSS_SELECTOR, ".txt_date").text
+
+    print(i+1, point, cmt, date, end="\n\n")
